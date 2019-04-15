e06f6d7dec986319153166e8cf8c65fa9882bafd
Toren Darby <darby@science.ubc.ca>
2019-04-15 14:30:23 -0700

    Make update_decks.py. Rename main to generate_overall_value.py

--- src/update_cards.py
+++ src/update_cards.py
@@ -1,5 +1,5 @@
 from src.card import CardLearner
-from src.main import file_prefix
+from src.generate_overall_value import file_prefix
 
 if __name__ == '__main__':
     card_learner = CardLearner(file_prefix)
--- src/update_decks.py
+++ src/update_decks.py
@@ -1,6 +1,9 @@
-# from deck import DeckLearner
-# from main import file_prefix
+from src.deck import DeckLearner
+from src.deck_searches import get_deck_searches
+from src.generate_overall_value import file_prefix
 
-# if __name__ == '__main__':
-#     deck_learner = DeckLearner(file_prefix)
-#     deck_learner.update()
+if __name__ == '__main__':
+    deck_searches = get_deck_searches()
+    for deck_search in deck_searches:
+        deck_learner = DeckLearner(file_prefix, deck_search)
+        deck_learner.update()
