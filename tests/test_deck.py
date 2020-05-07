1addb2a5f3727f02f912c4838c9b1634ae897ea6
Toren Darby <darby@science.ubc.ca>
2020-05-07 01:39:43 -0700

    Remove ignorable files

--- tests/test_deck.py
+++
@@ -1,5 +0,0 @@
-# from unittest import TestCase
-#
-#
-# class TestDeck(TestCase):
-#     pass
--- tests/test_rewards.py
+++
@@ -1,6 +0,0 @@
-class TestCardClass:
-    def test_num_cards(self):
-        assert False
-
-    def test_get_value(self):
-        assert False
