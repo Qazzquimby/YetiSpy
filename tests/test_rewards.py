41fd27cdb733aee021af0cf37d60ce96b20c74ed
Toren Darby <darby@science.ubc.ca>
2020-08-03 14:56:18 -0700

    Remove unmade rewards tests

--- tests/test_rewards.py
+++ tests/test_rewards.py
@@ -4,11 +4,3 @@ import rewards
 def test_something():
     rewards.DRAFT_PACK.get_value()
     assert True
-
-
-# def test_change_card_classes_to_card_classes_with_amounts():
-#     assert False
-#
-#
-# def test_get_value():
-#     assert False
