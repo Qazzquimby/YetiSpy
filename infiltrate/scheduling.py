f6bb7828d9ee5cc9b2aaf8332dd93d865ef2c431
Toren Darby <darby@science.ubc.ca>
2020-08-11 00:44:47 -0700

    Cleanup scheduling updates

--- infiltrate/scheduling.py
+++ infiltrate/scheduling.py
@@ -8,18 +8,31 @@ import models.card
 import models.card_set
 import models.deck
 import models.deck_search
+import models.user
+
+UPDATES_TO_INTERVALS = {
+    models.card.update_cards: 3,
+    models.card_set.update: 3,
+    models.deck.update_decks: 3,
+    models.deck_search.update_deck_searches: 3,
+}
+
+
+def initial_update():
+    models.card.db.create_all()
+    models.card.db.session.commit()
+    # models.card_set.CardSetName.query.delete()
+    if len(models.card_set.CardSetName.query.all()) == 0:
+        for update in UPDATES_TO_INTERVALS.keys():
+            update()
 
 
 def schedule_updates():
     """Schedules tasks"""
     # Todo move the details to a config file.
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=models.card.update_cards, trigger="interval", days=3)
-    scheduler.add_job(func=models.card_set.update, trigger="interval", days=3)
-    scheduler.add_job(func=models.deck.update_decks, trigger="interval", days=3)
-    scheduler.add_job(
-        func=models.deck_search.update_deck_searches, trigger="interval", days=3
-    )
+    for update, interval in UPDATES_TO_INTERVALS.items():
+        scheduler.add_job(func=update, trigger="interval", days=interval)
     scheduler.start()
 
     # Shut down the scheduler when exiting the app
@@ -27,7 +40,7 @@ def schedule_updates():
 
 
 if __name__ == "__main__":
-    # models.card.update_cards()
+    models.card.update_cards()
     # models.card_set.update()
-    models.deck.update_decks()
-    models.deck_search.update_deck_searches()
+    # models.deck.update_decks()
+    # models.deck_search.update_deck_searches()
