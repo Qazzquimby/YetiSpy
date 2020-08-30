1ed20e71cbdf06c7588024d18a75d817860e5fd8
Toren Darby <darby@science.ubc.ca>
2020-08-30 14:49:35 -0700

    Replace app with hello world to debug

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-"""
-This is invoked to start a production server.
-"""
-import sys
+# """
+# This is invoked to start a production server.
+# """
+# import sys
+#
+# sys.path.append("/opt/python/current/app/infiltrate")
+# from infiltrate import application, setup_application
+#
+# app = application
+#
+# if __name__ == "__main__":
+#     setup_application(app)
+#     app.run()
 
-sys.path.append("/opt/python/current/app/infiltrate")
-from infiltrate import application, setup_application
 
-app = application
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello world..."
+
 
 if __name__ == "__main__":
-    setup_application(app)
     app.run()
