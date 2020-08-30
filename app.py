e1021bd6eb6b5c6fa1d7cce1c78b83eab47731a6
Toren Darby <darby@science.ubc.ca>
2020-08-30 13:43:52 -0700

    rename application to app to meet gunicorn spec

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import sys
 sys.path.append("/opt/python/current/app/infiltrate")
 from infiltrate import application, setup_application
 
+app = application
+
 if __name__ == "__main__":
-    setup_application(application)
-    application.run()
+    setup_application(app)
+    app.run()
