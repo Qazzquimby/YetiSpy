491b5afcb7968e3b0d0de6da5733a927b56a3c5e
Toren Darby <darby@science.ubc.ca>
2020-08-30 15:01:59 -0700

    Try gunicorn without renaming application

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-# """
-# This is invoked to start a production server.
-# """
-# import sys
-#
-# sys.path.append("/opt/python/current/app/infiltrate")
-# from infiltrate import application, setup_application
-#
-# app = application
-#
-# if __name__ == "__main__":
-#     setup_application(app)
-#     app.run()
+"""
+This is invoked to start a production server.
+"""
+import sys
 
-
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello world..."
-
+sys.path.append("/opt/python/current/app/infiltrate")
+from infiltrate import application, setup_application
 
 if __name__ == "__main__":
-    app.run()
+    setup_application(application)
+    application.run()
+
+# from flask import Flask, render_template, request
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def index():
+#     return "Hello world..."
+#
+#
+# if __name__ == "__main__":
+#     app.run()
