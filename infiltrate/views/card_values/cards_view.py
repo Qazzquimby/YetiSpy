84476105386b8742db42c527d979635c6bbca6b6
Toren Darby <darby@science.ubc.ca>
2020-09-03 14:07:48 -0700

    Change ranking #s to be part of the data rather than got from the page number and index.
    Allows rankings in search results

--- infiltrate/views/card_values/cards_view.py
+++ infiltrate/views/card_values/cards_view.py
@@ -6,7 +6,6 @@ from flask_classy import FlaskView
 import infiltrate.models.card.completion as completion
 import infiltrate.views.card_values.card_displays as card_displays
 import infiltrate.views.card_values.display_filters as display_filters
-import infiltrate.global_data as global_data
 
 
 class CardsView(FlaskView):
@@ -21,17 +20,9 @@ class CardsView(FlaskView):
     def card_values(self, page_num=0, sort_str="efficiency", owner_str=None):
         """A table loaded into the card values page."""
 
+        displays = self._get_displays(sort_str, owner_str)
+
         page_num = int(page_num)
-        sort = display_filters.get_sort(sort_str)
-        if not owner_str:
-            ownership = sort.default_ownership
-        else:
-            ownership = display_filters.get_owner(owner_str)
-
-        displays = card_displays.CardDisplays.make_for_user(flask_login.current_user)
-
-        displays = displays.configure(sort, ownership)
-
         page_num, cards_on_page = displays.get_page(page_num)
 
         scaled = (np.log2(cards_on_page["play_craft_efficiency"] * 100) * 15).clip(
@@ -47,17 +38,16 @@ class CardsView(FlaskView):
             cards_per_page=card_displays.CardDisplays.CARDS_PER_PAGE,
         )
 
-    def card_search(self, search_str="_"):
+    def card_search(self, search_str="_", sort_str="efficiency", owner_str=None):
         """Searches for cards with names matching the search string,
         by the method used in AllCards"""
 
-        user = flask_login.current_user
-        displays = card_displays.CardDisplays.make_for_user(user)
+        displays = self._get_displays(sort_str, owner_str)
 
         search_str = search_str[1:]
         search_str = search_str.lower()
         matching_card_df = completion.get_matching_card(
-            displays.value_info, user, search_str
+            displays.value_info, flask_login.current_user, search_str
         )
 
         return flask.render_template(
@@ -67,3 +57,15 @@ class CardsView(FlaskView):
             card_values=matching_card_df,
             cards_per_page=16,
         )
+
+    def _get_displays(self, sort_str="efficiency", owner_str=None):
+        sort = display_filters.get_sort(sort_str)
+        if not owner_str:
+            ownership = sort.default_ownership
+        else:
+            ownership = display_filters.get_owner(owner_str)
+
+        displays = card_displays.CardDisplays.make_for_user(flask_login.current_user)
+        displays = displays.configure(sort, ownership)
+        displays.value_info["rank"] = np.arange(1, len(displays.value_info) + 1)
+        return displays
