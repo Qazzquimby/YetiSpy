0e15310ba6f39088590a13c94b8b90cc869aa6c9
Toren Darby <darby@science.ubc.ca>
2021-01-14 19:25:11 -0800

    Update draft.py and add expedition.py

--- infiltrate/models/card/draft.py
+++ infiltrate/models/card/draft.py
@@ -1,49 +1,35 @@
 import typing as t
 
+import infiltrate.eternal_warcy_cards_browser as ew_cards
 import infiltrate.browsers as browsers
 from infiltrate import db
 from infiltrate.models.card import Card, CardId
 
 
-def update_draft_pack_contents():
+def update_is_in_draft_pack():
     """Sets the is_in_draft_pack column of the cards table
     to match Eternal Warcry readings."""
-    draft_card_ids = _get_draft_pack_card_ids()
     Card.query.update({"is_in_draft_pack": False})
 
+    draft_card_ids = _get_draft_pack_card_ids()
     for card_id in draft_card_ids:
-        (
-            Card.query.filter(
-                Card.set_num == card_id.set_num, Card.card_num == card_id.card_num
-            ).update({"is_in_draft_pack": True})
-        )
+        Card.query.filter(
+            Card.set_num == card_id.set_num, Card.card_num == card_id.card_num
+        ).update({"is_in_draft_pack": True})
     db.session.commit()
 
 
-def most_recent_draft_pack_json_url():
-    file_name_selector = (
-        "#body-wrapper > div > div > div:nth-child(2) > div > a:nth-child(1)"
-    )
-    file_name = browsers.get_first_text_from_url_and_selector(
-        "https://eternalwarcry.com/cards/download", file_name_selector
-    ).strip()
-    return f"https://eternalwarcry.com/content/draftpacks/{file_name}"  # Todo, this is wrong. Gives first alphabetical, not latest.
-
-
-def most_recent_draft_pack_cards_url():
-    draft_pack_id_selector = "#DraftPack > option:nth-child(2)"
-    draft_pack_id = browsers.get_first_element_from_url_and_selector(
-        "https://eternalwarcry.com/cards", draft_pack_id_selector
-    ).attrs["value"]
-    url = f"https://eternalwarcry.com/cards?DraftPack={draft_pack_id}"
-    return url
-
-
 def _get_draft_pack_card_ids() -> t.List[CardId]:
-    draft_pack_url = most_recent_draft_pack_json_url()
-    newest_draft_pack = browsers.get_json_from_url(draft_pack_url)
-    card_ids = []
-    for card in newest_draft_pack:
-        card_id = CardId(set_num=card["SetNumber"], card_num=card["EternalID"])
-        card_ids.append(card_id)
-    return card_ids
+    draft_pack_id = _get_draft_pack_id()
+    root_url = ew_cards.get_ew_cards_root_url(draft_pack_id=draft_pack_id)
+    return ew_cards.get_card_ids_in_search(root_url)
+
+
+def _get_draft_pack_id():
+    card_url = "https://eternalwarcry.com/cards"
+    most_recent_expedition_selector = "#DraftPack > option:nth-child(2)"
+    element = browsers.get_first_element_from_url_and_selector(
+        url=card_url, selector=most_recent_expedition_selector
+    )
+    draft_pack_id = element.attrs["value"]
+    return draft_pack_id
---
+++ infiltrate/models/card/expedition.py
@@ -0,0 +1,42 @@
+import typing as t
+
+import infiltrate.browsers as browsers
+import infiltrate.eternal_warcy_cards_browser as ew_cards
+import infiltrate.models.card as card_mod
+from infiltrate import db
+
+
+def update_is_in_expedition():
+    """Sets the is_in_expedition column of the cards table
+    to match Eternal Warcry readings."""
+    card_mod.Card.query.update({"is_in_expedition": False})
+
+    expedition_card_ids = _get_expedition_card_ids()
+    for card_id in expedition_card_ids:
+        card_mod.Card.query.filter(
+            card_mod.Card.set_num == card_id.set_num,
+            card_mod.Card.card_num == card_id.card_num,
+        ).update({"is_in_expedition": True})
+    db.session.commit()
+
+
+def _get_expedition_card_ids() -> t.List[card_mod.CardId]:
+    expedition_id = _get_expedition_id()
+    root_url = ew_cards.get_ew_cards_root_url(expedition_id=expedition_id)
+    return ew_cards.get_card_ids_in_search(root_url)
+
+
+def _get_expedition_id():
+    card_url = "https://eternalwarcry.com/cards"
+    most_recent_expedition_selector = "#Expedition > option:nth-child(2)"
+    element = browsers.get_first_element_from_url_and_selector(
+        url=card_url, selector=most_recent_expedition_selector
+    )
+    expedition_id = element.attrs["value"]
+    return expedition_id
+
+
+if __name__ == "__main__":
+    result = _get_expedition_card_ids()
+
+    print("debug")
