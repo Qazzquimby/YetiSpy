971c4b365238ac076079770111444c840e4655ab
Toren Darby <torendarby@gmail.com>
2022-11-15 14:24:34 -0800

    Revert "feat: Use expedition vault instead of expedition column"
    This reverts commit 56c870f07879f280d9670a231ab7e503d44a2f26.

--- infiltrate/models/card/expedition.py
+++ infiltrate/models/card/expedition.py
@@ -6,39 +6,35 @@ import infiltrate.models.card as card_mod
 from infiltrate import db
 
 
-def update_is_in_expedition_vault():
+def update_is_in_expedition():
     """Sets the is_in_expedition column of the cards table
     to match Eternal Warcry readings."""
-    card_mod.Card.query.update({"is_in_expedition_vault": False})
+    card_mod.Card.query.update({"is_in_expedition": False})
 
-    expedition_vault_card_ids = _get_expedition_vault_card_ids()
-    for card_id in expedition_vault_card_ids:
+    expedition_card_ids = _get_expedition_card_ids()
+    for card_id in expedition_card_ids:
         card_mod.Card.query.filter(
             card_mod.Card.set_num == card_id.set_num,
             card_mod.Card.card_num == card_id.card_num,
-        ).update({"is_in_expedition_vault": True})
+        ).update({"is_in_expedition": True})
     db.session.commit()
 
 
-def _get_expedition_vault_card_ids() -> t.List[card_mod.CardId]:
-    vault_id = _get_expedition_vault_id()
-    root_url = ew_cards.get_ew_cards_root_url(vault_or_reprint_id=vault_id)
+def _get_expedition_card_ids() -> t.List[card_mod.CardId]:
+    expedition_id = _get_expedition_id()
+    root_url = ew_cards.get_ew_cards_root_url(expedition_id=expedition_id)
     return ew_cards.get_card_ids_in_search(root_url)
 
 
-def _get_expedition_vault_id():
+def _get_expedition_id():
     card_url = "https://eternalwarcry.com/cards"
-    vault_or_reprint_options = browsers.get_elements_from_url_and_selector(
-        url=card_url, selector="#DraftPack > option"
-    )[1:]
-    vaults = [
-        option
-        for option in vault_or_reprint_options
-        if "Expedition Vault" in option.text
-    ]
-    first_vault = vaults[0]
-    return first_vault.attrs["value"]
+    most_recent_expedition_selector = "#Expedition > option:nth-child(2)"
+    element = browsers.get_first_element_from_url_and_selector(
+        url=card_url, selector=most_recent_expedition_selector
+    )
+    expedition_id = element.attrs["value"]
+    return expedition_id
 
 
 if __name__ == "__main__":
-    result = _get_expedition_vault_card_ids()
+    result = _get_expedition_card_ids()
